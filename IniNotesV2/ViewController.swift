bbec8cfdf787c48c4d95fb67a29eea14b9844c44
Indratdev <indrat.dev@gmail.com>
2020-09-19 20:30:03 +0700

    Initial Commit

---
+++ IniNotesV2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IniNotesV2
+//
+//  Created by IndratS on 19/09/20.
+//  Copyright © 2020 IndratSaputra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
